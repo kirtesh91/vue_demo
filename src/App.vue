bb1989b607656e2adc5209243ee40cd6420c8bcd
Kirtesh Dusane <kirteshdusane91@gmail.com>
2020-04-03 14:37:37 +0530

    Form Build

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,97 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+    .form-app-container
+        .form-app
+            .form-section
+                form(@submit.prevent="submit")
+                    h2.title    Student Details
+                    .form
+                        .form-inputs
+                            input(v-model="details.name" placeholder="Enter Name")
+                            input(v-model="details.mobile" placeholder="Enter Mobile")
+                            input(v-model="details.email" placeholder="Enter Email")
+                        button(type="submit")    Submit
+            .students-data
+                CustomTable(:students="students")
 </template>
 
+<script>
+import CustomTable from "./components/CustomTable";
+export default {
+    components: { CustomTable },
+    data() {
+        return {
+            details: {
+                name: null,
+                mobile: null,
+                email: null
+            },
+            students: []
+        };
+    },
+    watch: {
+        "details.name": function(newVal) {
+            console.log(`Name Changed:${newVal}`);
+        }
+    },
+    methods: {
+        submit() {
+            if (
+                !this.details.name ||
+                !this.details.mobile ||
+                !this.details.email
+            ) {
+                alert("All the Details Are Compulsory!");
+                return;
+            }
+
+            this.students.push(this.details);
+            this.reset();
+        },
+        reset() {
+            this.details = {
+                name: null,
+                mobile: null,
+                email: null
+            };
+        }
+    },
+    name: "App"
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "./scss/app";
+.form-app-container {
+    min-height: 100vh;
+    background: #333;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    .form-app {
+        width: 75%;
     }
-  }
+
+    .form-section {
+        border-radius: 0.8rem;
+        background: #444;
+        color: #fff;
+        width: 100%;
+        margin-bottom: 1rem;
+        padding: 1rem;
+
+        .title {
+            margin-bottom: 1rem;
+        }
+
+        .form {
+            text-align: center;
+            .form-inputs {
+                padding: 0.5rem 1rem;
+                display: flex;
+                justify-content: center;
+            }
+        }
+    }
 }
 </style>
---
+++ src/components/CustomTable.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+    .students-data
+        h2.title    Student Data
+        transition(name="fade" mode="out-in" appear)
+            p.message(v-if="!students.length")   Sorry, No Details Available Yet
+            table(v-else)
+                thead
+                    tr
+                        th  No
+                        th Name
+                        th Mobile
+                        th Email
+                tbody
+                    tr(v-for="(student,index) in students")
+                        td  {{index + 1}}
+                        td  {{student.name}}
+                        td      {{student.mobile}}
+                        td  {{student.email}}
+</template>
+
+<script>
+export default {
+    props: {
+        students: Array
+    },
+    name: "CustomTable"
+};
+</script>
+
+<style lang="scss" scoped>
+.students-data {
+    border-radius: 0.8rem;
+    background: #444;
+    color: #fff;
+    padding: 1rem;
+    text-align: center;
+
+    .title {
+        margin-bottom: 1rem;
+    }
+
+    .message {
+        font-size: 14px;
+        text-transform: uppercase;
+    }
+}
+</style>
